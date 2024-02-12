f69d47c339712b98c851f13050991eae26f36e77
Abcdiregter <111136892+Abcdiregter@users.noreply.github.com>
2024-02-12 13:41:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -19,8 +19,22 @@ version : 0.1
 
 driver = webdriver.Google()
 driver.get("https://www.facebook.com/")
+
 usr = driver.find_element(By.NAME, "email")
 usr.send_keys(f{username})
+
+pwd = driver.find_element(By.NAME, "pass")
+pwd.send_keys(f'{password})
+
+login = driver.find_element(By.NAME, "login")
+login.click()
+
+if login.response_statuscode == 200 :
+   print("DANG NHAP THANH CONG VAO FACEBOOK")
+else :
+   print("KHONG DANG NHAO DUOC VAO FACEBOOK, CHECK TKMK")
+
+
 
 
 
