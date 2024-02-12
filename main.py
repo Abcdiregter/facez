5dfb083a47ccd43c56816811fa8b162db3216089
Abcdiregter <111136892+Abcdiregter@users.noreply.github.com>
2024-02-12 13:35:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+from selenium import webdriver
+import subporcess
+from config import username, password
+
+banner = ("""
+┃┏━━━┓┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┓┃┃┏┓┏┓┃
+┃┃┏━━┛┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┗┓┏┛┃┛┃┃
+┃┃┗━━┓━━┓┃━━┓━━┓━┓┃┃┃┓┃┃┏┛┓┃┃
+┃┃┏━━┛┃┓┃┃┏━┛┏┓┃┏┛┃┃┃┃┗┛┃┃┃┃┃
+┏┛┗┓┃┃┗┛┗┓┗━┓┃━┫┃┃┃┃┃┗┓┏┛┃┛┗┓
+┗━━┛┃┃━━━┛━━┛━━┛┛┃┃┃┃┃┗┛┃┃━━┛
+┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃
+┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃┃
+
+coded by hphong 
+version : 0.1
+
+""')
+
+driver = webdriver.Google()
+driver.get("https://www.facebook.com/")
+usr = driver.find_element(By.NAME, "email")
+usr.send_keys(f{username})
+
+
 
